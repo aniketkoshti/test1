4dbe55fcd434a13a4569dfc2a0aad795e0a9764f
aniketkoshti <167079763+aniketkoshti@users.noreply.github.com>
2024-09-20 11:19:07 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,17 @@
+package P3;
+
+import java.util.Scanner;
+
+public class Bank {
+	
+	
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the amount :");
+	int a=sc.nextInt();
+	
+	
+	
+}
+}
